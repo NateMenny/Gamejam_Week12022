4060f340a5029c519243d1bd8a2abbeae4075ce6
Nathanial <93955352+NateMenny@users.noreply.github.com>
2022-01-05 10:57:10 -0500

    Fixed Accel and Decel
    Character rigidbody now accelerates and decelerates with axis input

--- Re_GameJam/Assets/PlayerMovement2D.cs
+++ Re_GameJam/Assets/PlayerMovement2D.cs
@@ -10,72 +10,39 @@ public class PlayerMovement2D : MonoBehaviour
     public KeyCode moveRight;
     public KeyCode moveUp;
     public KeyCode moveDown;
+
+    // Movement checks
+    bool isMoving;
+   
     
     // 
     Rigidbody2D rb2d;
-    [SerializeField] float moveSpeed;
+    [SerializeField] float accel;
+    float maxVel;
+    float axisX;
+    float axisY;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (moveLeft == KeyCode.None)
-        {
-            moveLeft = KeyCode.A;
-        }
-        if (moveRight == KeyCode.None)
-        {
-            moveRight = KeyCode.D;
-        }
-        if (moveUp == KeyCode.None)
-        {
-            moveUp = KeyCode.W;
-        }
-        if (moveDown == KeyCode.None)
-        {
-            moveDown = KeyCode.S;
-        }
-
         rb2d = GetComponent<Rigidbody2D>();
-        moveSpeed = 1f;
-       
+        if(accel <= 0f) accel = 4f;
+        if (maxVel <= 0f) maxVel = 12f;
+        rb2d.drag = accel * 0.25f;
     }
 
-    // Update is called once per frame
+    // Update is called once per frames
     void Update()
     {
-        if (Input.GetKey(moveRight))
+        axisX = Input.GetAxisRaw("Horizontal");
+        axisY = Input.GetAxisRaw("Vertical");
+   
+        // we need something to zero the movement when no movement keys are down
+        rb2d.AddForce(new Vector2(axisX, axisY).normalized * accel);
+        if(rb2d.velocity.magnitude > maxVel)
         {
-            rb2d.AddForce(Vector2.right * moveSpeed);
+            rb2d.velocity = rb2d.velocity.normalized* maxVel;
         }
-        if (Input.GetKey(moveLeft))
-        {
-            rb2d.AddForce(Vector2.left * moveSpeed);
-        }
-        if (Input.GetKey(moveUp))
-        {
-            rb2d.AddForce(Vector2.up * moveSpeed);
-        }
-        if (Input.GetKey(moveDown))
-        {
-            rb2d.AddForce(Vector2.down * moveSpeed);
-        }
-         // we need something to zero the movement when no movement keys are down  
-        if (!Input.GetKey(moveRight))
-        {
-            rb2d.AddForce(Vector2.right * -moveSpeed);
-        }
-        if (!Input.GetKey(moveLeft))
-        {
-            rb2d.AddForce(Vector2.left * -moveSpeed);
-        }
-        if (!Input.GetKey(moveUp))
-        {
-            rb2d.AddForce(Vector2.up * -moveSpeed);
-        }
-        if (!Input.GetKey(moveDown))
-        {
-            rb2d.AddForce(Vector2.down * -moveSpeed);
-        }
-    // we need something to zero the movement when no movement keys are down 
+        
     }
 }
