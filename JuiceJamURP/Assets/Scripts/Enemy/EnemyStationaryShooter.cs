b90f231be48c1007af7a9ca70c3eb2456961e314
Nathanial <93955352+NateMenny@users.noreply.github.com>
2022-01-10 21:51:31 -0500

    Update EnemyStationaryShooter.cs
    Added shoot sound variable and plays on shot

--- JuiceJamURP/Assets/Scripts/Enemy/EnemyStationaryShooter.cs
+++ JuiceJamURP/Assets/Scripts/Enemy/EnemyStationaryShooter.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(ProjectileLauncher))]
+[RequireComponent(typeof(ProjectileLauncher), typeof(MainSounds))]
 public class EnemyStationaryShooter : Enemy
 {
+    public AudioClip shootSFX;
     ProjectileLauncher pl;
+
     // Start is called before the first frame update
     public override void Death(int drops)
     {
@@ -16,17 +18,19 @@ public class EnemyStationaryShooter : Enemy
     public override void Start()
     {
         pl = GetComponent<ProjectileLauncher>();
+        ms = GetComponent<MainSounds>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 direction = GameManager.instance.playerInstance.transform.position - transform.position;
-       Shoot(direction);
+        Shoot(direction);
     }
+
     void Shoot(Vector2 direction)
     {
         pl.LaunchProjectileToward(direction);
-        ms.Play(shootClip);
+        ms.Play(shootSFX);
     }
 }
